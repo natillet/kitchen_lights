360c6cc72cf354ce5a604ef80de82ea38b3d6cf4
Elene <natille@gmail.com>
2017-03-19 21:31:05 -0400

    Functional

---
+++ kitchen_lights.h
@@ -0,0 +1,12 @@
+#ifndef KITCHENLIGHTS_H
+#define KITCHENLIGHTS_H
+
+#define DBG_PRINTS 1
+
+#ifdef DBG_PRINTS
+#define DBG_PRINTLN(x) Serial.println(x)
+#define DBG_PRINT(x) Serial.print(x)
+#endif
+
+#endif //KITCHENLIGHTS_H
+
